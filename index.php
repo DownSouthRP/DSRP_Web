89d1245df5237bed669a93ee632f3521cac30b47
andybrown833 <60363355+andybrown833@users.noreply.github.com>
2020-09-12 15:52:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+
+$mailTo = 'andersonbrown833@gmail.com';
+$mailSubject = "Confirmation Code";
+$mailLink = 'https://' . $_SERVER['HTTP_HOST'] . '/home/auth/confirm.php?e=' . '-email-' . '&h=' . '-hash-';
+$mailTxt = "Hello, thank you for registering for dsrp.online." . "Head over to " . $mailLink . " to finish your registration.";
+$mailHeaders = "From: <REG@DSRP.ONLINE>";
+
+if(mail($mailTo,$mailSubject,$mailTxt,$mailHeaders)) {
+    exit;
+}
+
+
 echo '<script type="text/javascript">location.href = "/home";</script>';
 exit;
 ?>
