b8bb47e9701d6f1af05a19f5c34d0ca1011c9bc3
andybrown833 <60363355+andybrown833@users.noreply.github.com>
2020-09-12 13:27:52 -0500

    Update check.php

--- home/auth/check.php
+++ home/auth/check.php
@@ -9,8 +9,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/sys/design/pageReq.php";
 $mailTo = 'andersonbrown833@gmail.com';
 $mailSubject = "Registration Complete";
 $mailTxt = "Thank you for registering for dsrp.online. <a href='www.dsrp.online/home/auth/login.php'>CLICK HERE</a> to signin with the email and password you used to register.";
-$mailHeaders = "From: <REGISTRATION@DSRP.ONLINE> . '\r\n'
-Content-Type: text/html;";
+$mailHeaders = "From: <REGISTRATION@DSRP.ONLINE>" . '\r\n Content-Type: text/html;';
 
 if(mail($mailTo,$mailSubject,$mailTxt,$mailHeaders)) {
 	exit;
